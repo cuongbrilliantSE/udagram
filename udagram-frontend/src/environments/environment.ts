3eaea4163c550be73b2696f2c71967de191b49cf
cuongdoan <cuongdoan.brilliant@gmail.com>
2023-04-19 00:26:13 +0700

    reset env

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://abaee18c1705a4613a0d2e075d89941b-1992457327.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://af447264eae0b4f5da1c3c04756b3765-487646157.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
